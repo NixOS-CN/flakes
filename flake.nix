8e5868f7601ee2ff335d81e323b59388f5407c41
mlatus <wqseleven@gmail.com>
2021-01-19 16:00:13 +0800

    Re-export registries' outputs
    Re-export packages, legacyPackages of each registry's outpus (if any)

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     with builtins;
     let
       defaultSystem = "x86_64-linux";
+
       listFiles = dir: map (n: dir + "/${n}") (attrNames (readDir dir));
       listNixFilesRecursive = dir:
         flatten (mapAttrsToList (name: type:
@@ -23,24 +24,25 @@
             path
           else
             [ ]) (readDir dir));
+
       filterBySystem = system: pkgs:
-        filterAttrs (_: p:
-          if hasAttrByPath [ "meta" "platforms" ] p then
+        filterAttrsRecursive (_: p:
+          !(isDerivation p) || (if hasAttrByPath [ "meta" "platforms" ] p then
             elem system p.meta.platforms
           else
-            system == defaultSystem) pkgs;
+            system == defaultSystem)) pkgs;
       getPackages = f: dir:
         listToAttrs (map (name: {
           inherit name;
           value = f (dir + "/${name}");
         }) (attrNames (readDir dir)));
-
       makePackageSet = f:
         getPackages f ./top-level // listToAttrs (map (dir: {
           name = baseNameOf dir;
           value = recurseIntoAttrs (getPackages f dir);
         }) (listFiles ./package-set));
       makePackageSet' = system: f: filterBySystem system (makePackageSet f);
+
       toNixOSCNRegistries = mapAttrs (name: entry: {
         from = {
           id = "nixos-cn";
@@ -49,6 +51,25 @@
         };
         to = entry;
       });
+      # Currently we only have github/gitlab registries
+      registryEntryToGitURL = entry:
+        with entry;
+        "https://${type}.com/${owner}/${repo}.git";
+      registryEntryToFlakeURL = entry: lock:
+        with entry;
+        "${type}:${owner}/${repo}/${lock.rev}";
+      registryOutputs = let
+        locks = importJSON ./registries.lock;
+        registries = import ./registries.nix;
+      in mapAttrs (name: entry:
+        (getFlake (registryEntryToFlakeURL entry locks.${name})).outputs)
+      registries;
+      extractFromRegistries = f:
+        filterAttrs (_: v: v != { }) (mapAttrs f registryOutputs);
+      mapRecurseIntoAttrs = s:
+        mapAttrs (_: v:
+          if !isAttrs v || isDerivation v then v else mapRecurseIntoAttrs v)
+        (recurseIntoAttrs s);
 
     in eachDefaultSystem (system:
       let
@@ -57,11 +78,33 @@
           config.allowUnfree = true;
         });
       in rec {
-        legacyPackages = makePackageSet' system (n: pkgs.callPackage n { });
+        legacyPackages = makePackageSet' system (n: pkgs.callPackage n { }) // {
+          re-export = mapRecurseIntoAttrs (extractFromRegistries (_: output:
+            (attrByPath [ "packages" system ] { } output)
+            // (attrByPath [ "legacyPackages" system ] { } output)));
+        };
         checks = flattenTree legacyPackages;
+        apps = {
+          update-lock = mkApp {
+            drv = with pkgs;
+              writeShellScriptBin "update-lock" ''
+                export PATH=${makeBinPath [ git coreutils jq ]}
+                function getRev(){
+                  git ls-remote $1 HEAD|cut -f1
+                }
+                lock='{}'
+                ${concatStrings (mapAttrsToList (name: entry: ''
+                  lock=$(echo "$lock"|jq -c ".\"${name}\".rev = \"$(getRev "${
+                    registryEntryToGitURL entry
+                  }")\"")
+                '') (import ./registries.nix))}
+                echo "$lock"|jq
+              '';
+          };
+        };
       }) // {
         overlay = final: prev: {
-          nixoscn = makePackageSet (n: final.callPackage n { });
+          nixoscn = recurseIntoAttrs (makePackageSet (n: final.callPackage n { }));
         };
         nixosModules.nixoscn = { ... }: {
           nixpkgs.overlays = [ self.overlay ];
