1a9e2fbf14c5a932848d4e87a4f98ae86feb6e10
mlatus <wqseleven@gmail.com>
2021-01-21 17:37:20 +0800

    update ibus-rime

--- top-level/ibus-rime/default.nix
+++ top-level/ibus-rime/default.nix
@@ -3,32 +3,33 @@
 
 stdenv.mkDerivation rec {
   pname = "ibus-rime";
-  version = "2020-09-29";
+  version = "2020-12-04";
 
   src = fetchFromGitHub {
     owner = "rime";
     repo = "ibus-rime";
-    rev = "933ea96c172715511efa6c4601af73fa2b9ab236";
-    sha256 = "1d4kins5pjgvagsphncpcsldgw356c8axyxi66g4kgvfsshsrggy";
+    rev = "bfabe6722c1e20ee1ba33aa5107ed74934252705";
+    sha256 = "sha256-KTlK5e6W6PZLZX5K3hDbTX6C2/Y246uTUzodqJEbk1k=";
   };
 
   buildInputs = [ gdk-pixbuf glib ibus libnotify librime brise ];
   nativeBuildInputs = [ cmake pkgconfig ];
+  cmakeFlags = [ "-DRIME_DATA_DIR=${brise}/share/rime-data" ];
 
-  makeFlags = [ "PREFIX=$(out)" ];
-  dontUseCmakeConfigure = true;
+  # makeFlags = [ "PREFIX=$(out)" ];
+  # dontUseCmakeConfigure = true;
 
-  prePatch = ''
-    substituteInPlace Makefile \
-       --replace 'cmake' 'cmake -DRIME_DATA_DIR=${brise}/share/rime-data'
-     substituteInPlace rime_config.h \
-       --replace '/usr' $out
-     substituteInPlace rime_config.h \
-       --replace 'IBUS_RIME_SHARED_DATA_DIR IBUS_RIME_INSTALL_PREFIX' \
-                 'IBUS_RIME_SHARED_DATA_DIR "${brise}"'
-     substituteInPlace rime.xml \
-       --replace '/usr' $out
-  '';
+  # prePatch = ''
+  #   substituteInPlace Makefile \
+  #      --replace 'cmake' 'cmake -DRIME_DATA_DIR=${brise}/share/rime-data'
+  #    substituteInPlace rime_config.h \
+  #      --replace '/usr' $out
+  #    substituteInPlace rime_config.h \
+  #      --replace 'IBUS_RIME_SHARED_DATA_DIR IBUS_RIME_INSTALL_PREFIX' \
+  #                'IBUS_RIME_SHARED_DATA_DIR "${brise}"'
+  #    substituteInPlace rime.xml \
+  #      --replace '/usr' $out
+  # '';
 
   meta = {
     description = "Rime for Linux/IBus";
