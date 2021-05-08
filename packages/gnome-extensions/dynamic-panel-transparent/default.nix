54bae57394e2d646d07abc24894e94d600ba7fe6
mlatus <wqseleven@gmail.com>
2021-05-08 20:48:17 +0800

    update: dynamic-panel-transparent

--- packages/gnome-extensions/dynamic-panel-transparent/default.nix
+++ packages/gnome-extensions/dynamic-panel-transparent/default.nix
@@ -4,8 +4,8 @@ in stdenv.mkDerivation {
   name = "dynamic-panel-transparent";
   src = fetchgit {
     url = "https://github.com/rockon999/dynamic-panel-transparency.git";
-    rev = "0800c0a921bb25f51f6a5ca2e6981b1669a69aec";
-    sha256 = "0200mx861mlsi9lf7h108yam02jfqqw55r521chkgmk4fy6z99pq";
+    rev = "f9e720e98e40c7a2d87928d09a7313c9ef2e832c";
+    sha256 = "0njykxjiwlcmk0q8bsgqaznsryaw43fspfs6rzsjjz5p0xaq04nw";
   };
 
   buildPhase = ''
