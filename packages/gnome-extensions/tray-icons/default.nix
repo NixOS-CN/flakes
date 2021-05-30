d82c5c2364a25112fcf7d2f4f9a87b4fb4582b83
mlatus <wqseleven@gmail.com>
2021-05-31 00:54:19 +0800

    update: tray-icons

--- packages/gnome-extensions/tray-icons/default.nix
+++ packages/gnome-extensions/tray-icons/default.nix
@@ -4,8 +4,8 @@ in stdenv.mkDerivation {
   name = "dynamic-panel-transparent";
   src = fetchgit {
     url = "https://github.com/zhangkaizhao/gnome-shell-extension-tray-icons.git";
-    rev = "bde2b275f7afe4b4b252836ad73fd31b8aa5a9ac";
-    sha256 = "06wdyzikzbbij77id0zg6nxxjaddn9af9h2sjfbqh3kjz4nwj2z6";
+    rev = "82a1024bfff0eaa88891c938c0e37cd7857573c8";
+    sha256 = "0jsaxcw5hqwc1a98dc17nk4zi5x9bb68bhysaykf5zlv2i1x2x5w";
   };
 
   installPhase = ''
