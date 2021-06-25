e30f209c10e8645e34ae3d19814493917fd451c7
mlatus <wqseleven@gmail.com>
2021-06-25 16:07:19 +0800

    migrate pam-python to python3

--- packages/pam-python/default.nix
+++ packages/pam-python/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, python2, python2Packages, fetchurl, pam }:
+{ stdenv, python3, python3Packages, fetchurl, pam }:
 let outPath = placeholder "out";
 in stdenv.mkDerivation rec {
   pname = "pam-python";
@@ -8,10 +8,15 @@ in stdenv.mkDerivation rec {
       "https://downloads.sourceforge.net/project/pam-python/pam-python-1.0.8-1/pam-python-1.0.8.tar.gz";
     sha256 = "sha256-/GnXcX2wUJERUAqBBTSH+naE4b47fQritRlwtv3JGPY=";
   };
-  buildInputs = [ python2 python2Packages.sphinx pam ];
+  buildInputs = [ python3 python3Packages.sphinx pam ];
   preBuild = ''
+    substituteInPlace src/setup.py --replace '/usr/bin/python2' '/usr/bin/python3'
+    substituteInPlace src/test.py  --replace '/usr/bin/python2' '/usr/bin/python3'
     patchShebangs .
     substituteInPlace src/Makefile --replace '-Werror' '-O -Werror=cpp'
+    substituteInPlace src/Makefile --replace \
+      'cp build/lib.*/pam_python.so $(DESTDIR)$(LIBDIR)' \
+      'cp build/lib.*/pam_python.*.so $(DESTDIR)$(LIBDIR)/pam_python.so'
   '';
   makeFlags = [ "PREFIX=${outPath}" "LIBDIR=${outPath}/lib/security" ];
 
