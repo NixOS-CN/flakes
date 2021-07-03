f691858c04d08d2a6eab8b11898f86ffdaedb453
Automation - Ninlives <wqseleven@gmail.com>
2021-07-03 01:03:18 +0000

    Update packages/gnome-themes/gruvbox-dark-gtk - Automated Commit

--- packages/gnome-themes/gruvbox-dark-gtk/default.nix
+++ packages/gnome-themes/gruvbox-dark-gtk/default.nix
@@ -3,8 +3,8 @@ stdenv.mkDerivation rec {
   name = "gruvbox-dark-gtk";
   src = fetchgit {
     url = "https://github.com/jmattheis/gruvbox-dark-gtk";
-    rev = "9f46a26ac94585d44ebc4fadac40cec8210337e8";
-    sha256 = "1gn8bkcaqx21jdrpc2cxv50lfzx4245jvhsxsrln6k2cryikbbqb";
+    rev = "04221c1317bed7a765a09f5bb00a97765ce4a711";
+    sha256 = "0bfnzmjqg9hil2023l8b0cjd7rkmskw3gi6l4qz78cwkj9fymnhz";
   };
 
   dontBuild = true;
