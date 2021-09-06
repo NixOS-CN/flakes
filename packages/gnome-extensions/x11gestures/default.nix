39b69441b2ee7fae2f86146c1a85a30a06cd4480
Automation - Ninlives <wqseleven@gmail.com>
2021-09-06 01:13:03 +0000

    Update packages/gnome-extensions/x11gestures - Automated Commit

--- packages/gnome-extensions/x11gestures/default.nix
+++ packages/gnome-extensions/x11gestures/default.nix
@@ -3,8 +3,8 @@ stdenv.mkDerivation {
   name = "gnome-shell-extension-x11gestures";
   src = fetchgit {
     url = "https://github.com/JoseExposito/gnome-shell-extension-x11gestures.git";
-    rev = "6abd677270c6174dc006d000f064212a32584fd5";
-    sha256 = "0km87dxbj6xawagqfks5yz85dy23k4cdjzpz09ql1zi1hzf98hby";
+    rev = "22160502399fb12f262d1716425721dd03a416ca";
+    sha256 = "1rhg2m2wdxgszf2fpg77p738gjlfphwa75qmmyryr69k58lyqhx3";
   };
 
   nativeBuildInputs = [ glib.dev ];
