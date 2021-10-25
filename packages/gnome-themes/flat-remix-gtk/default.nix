dac1059e6a48e5ebc89dac10acd884976f6f6841
mlatus <wqseleven@gmail.com>
2021-10-25 15:41:52 +0800

    [gnome-themes.flat-remix-gtk] fix build and update version

--- packages/gnome-themes/flat-remix-gtk/default.nix
+++ packages/gnome-themes/flat-remix-gtk/default.nix
@@ -4,12 +4,10 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "daniruiz";
     repo  = "flat-remix-gtk";
-    rev   = "eafc1ae3a0332ae7cd69f28d66fa8f1848563757";
-    sha256 = "0fwwf43ibbqd1qy18508fkwx30n4jdydwavzb01swfhgm38iq4gw";
+    rev   = "58109e46012cc675d14c4cd74645fa262f0885cc";
+    sha256 = "0r0p7lp6gwjgg08rrf2k3d3xjrrk3nmzz0snk2rylpv15i1r6sym";
   };
-  installPhase = ''
-    mkdir -p $out/share/themes
-    cp -r Flat-Remix-* $out/share/themes
-  '';
+  dontBuild = true;
+  makeFlags = [ "DESTDIR=${placeholder "out"}" "PREFIX=" ];
   passthru.updateAction = "${update-nix-fetchgit}/bin/update-nix-fetchgit *";
 }
