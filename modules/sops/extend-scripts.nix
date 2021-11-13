9d76b25f81bf3b02f2433de03e283d1bbdf0ee61
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-11-13 19:07:26 +0800

    Fix wrong `mkIf` condition in sops/extend-scripts.nix module

--- modules/sops/extend-scripts.nix
+++ modules/sops/extend-scripts.nix
@@ -10,7 +10,8 @@ let
     (filterAttrs (_: v: !v.neededForUsers) config.sops.secrets) != { };
   hasUserSecrets = (filterAttrs (_: v: v.neededForUsers) config.sops.secrets)
     != { };
-in {
+in
+{
   options.sops.extendScripts = {
     pre-sops = scriptOption;
     pre-sops-install-secrets = scriptOption;
@@ -18,25 +19,27 @@ in {
     post-sops = scriptOption;
   };
 
-  config = optionalAttrs (options ? sops.secrets) (mkMerge [
-    (mkIf (builtins.any (s: s != "") (builtins.attrValues cfg)) {
-      system.activationScripts.pre-sops = noDepEntry cfg.pre-sops;
-      system.activationScripts.pre-sops-install-secrets =
-        stringAfter [ "pre-sops" ] cfg.pre-sops-install-secrets;
-      system.activationScripts.post-sops-install-secrets = stringAfter
-        ((optional hasUserSecrets "setupSecretsForUsers")
-          ++ (optional hasRegularSecrets "setupSecrets"))
-        cfg.post-sops-install-secrets;
-      system.activationScripts.post-sops =
-        stringAfter [ "post-sops-install-secrets" ] cfg.post-sops;
-    })
-    (mkIf hasUserSecrets {
-      system.activationScripts.setupSecretsForUsers.deps =
-        [ "pre-sops-install-secrets" ];
-    })
-    (mkIf hasRegularSecrets {
-      system.activationScripts.setupSecrets.deps =
-        [ "pre-sops-install-secrets" ];
-    })
-  ]);
+  config = optionalAttrs (options ? sops.secrets)
+    (mkIf (builtins.any (s: s != "") (builtins.attrValues cfg))
+      (mkMerge [
+        {
+          system.activationScripts.pre-sops = noDepEntry cfg.pre-sops;
+          system.activationScripts.pre-sops-install-secrets =
+            stringAfter [ "pre-sops" ] cfg.pre-sops-install-secrets;
+          system.activationScripts.post-sops-install-secrets = stringAfter
+            ((optional hasUserSecrets "setupSecretsForUsers")
+              ++ (optional hasRegularSecrets "setupSecrets"))
+            cfg.post-sops-install-secrets;
+          system.activationScripts.post-sops =
+            stringAfter [ "post-sops-install-secrets" ] cfg.post-sops;
+        }
+        (mkIf hasUserSecrets {
+          system.activationScripts.setupSecretsForUsers.deps =
+            [ "pre-sops-install-secrets" ];
+        })
+        (mkIf hasRegularSecrets {
+          system.activationScripts.setupSecrets.deps =
+            [ "pre-sops-install-secrets" ];
+        })
+      ]));
 }
