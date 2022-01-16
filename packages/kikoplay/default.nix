c14278a6008b2735e0a359a7f4d72db4e97c91e3
Automation - Ninlives <wqseleven@gmail.com>
2022-01-16 02:01:57 +0000

    Update packages/kikoplay - Automated Commit

--- packages/kikoplay/default.nix
+++ packages/kikoplay/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchFromGitHub, qt5, mpv, aria2, lua, update-nix-fetchgit }: stdenv.mkDerivation {
   pname = "kikoplay";
-  version = "unstable-2022-01-03";
+  version = "unstable-2022-01-16";
 
   src = fetchFromGitHub {
     owner = "Protostars";
     repo = "KikoPlay";
-    rev = "ec4537001f1b08a058f3369c994f014d5f9e5fc8";
-    sha256 = "0jgr9jmiw2gjyqrvkg50qddcqmz73zl5a4g5hypkmclmfv3v0dwl";
+    rev = "0151e12712539a6cfad1763f15d16e43e0268108";
+    sha256 = "0vaq9gswb2mrhdbisvadqyf7fsw6q3z7g4dnazj2x35w9ilcnq4v";
   };
 
   nativeBuildInputs = [ qt5.qmake qt5.wrapQtAppsHook ];
