487297d15abe8056c8d9e42b6e1d98bdac21b6d5
Automation - Ninlives <wqseleven@gmail.com>
2022-04-23 01:55:31 +0000

    Update packages/gnome-themes/flat-remix-gtk - Automated Commit

--- packages/gnome-themes/flat-remix-gtk/default.nix
+++ packages/gnome-themes/flat-remix-gtk/default.nix
@@ -4,8 +4,8 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "daniruiz";
     repo  = "flat-remix-gtk";
-    rev   = "2632b82d83f4fec00e33d2c68b36dd7d5dd9bc98";
-    sha256 = "0psvyghyzff8rmy3qzy6kpg9bg3zi0qaw5zd475dsg58qcz9i09c";
+    rev   = "9ef259884ca4ca5d0abd762d60d30b6b3d70be1c";
+    sha256 = "1qwj77hd3js3ihbh7mf5zqmprq03wh7813aryblir0jymr0qkf4c";
   };
   dontBuild = true;
   makeFlags = [ "DESTDIR=${placeholder "out"}" "PREFIX=" ];
