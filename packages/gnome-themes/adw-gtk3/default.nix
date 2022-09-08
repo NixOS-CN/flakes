8a27511422cfe2b9cc4e6226474d59ebf3803e74
Automation - Ninlives <wqseleven@gmail.com>
2022-09-08 02:36:29 +0000

    Update packages/gnome-themes/adw-gtk3 - Automated Commit

--- packages/gnome-themes/adw-gtk3/default.nix
+++ packages/gnome-themes/adw-gtk3/default.nix
@@ -3,8 +3,8 @@ stdenv.mkDerivation {
   name = "adw-gtk3";
   src = fetchgit {
     url = "https://github.com/lassekongo83/adw-gtk3.git";
-    rev = "6f453f489bed5c1fd80ad4978c34990815e301b4";
-    sha256 = "0hvh6bn27p4lyzyx96kqsw1pvx33pdi24hmrak6rkl94cr5siyzm";
+    rev = "a9af8b72d11f79eabb800fd3116d43ace1fb7a81";
+    sha256 = "0n5asdk77fx9m273hxrsvqm0iy7hby7csqyl5zpvjpz8bzr4jb4r";
   };
 
   installPhase = ''
